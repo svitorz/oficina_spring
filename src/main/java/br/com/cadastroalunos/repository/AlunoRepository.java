5508e3b4b051cc3d58778f49471f6e853050f025
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 10:21:28 -0300

    feat: crate alunos repository

---
+++ src/main/java/br/com/cadastroalunos/repository/AlunoRepository.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.cadastroalunos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.cadastroalunos.entity.Aluno;
+
+/**
+ *
+ * @author vitor
+ */
+@Repository
+public interface AlunoRepository extends JpaRepository<Long, Aluno> {
+
+}
