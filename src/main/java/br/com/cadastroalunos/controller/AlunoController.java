481d49bcdbaef3e6d567fd4e499d057a5e5a1233
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 09:24:54 -0300

    feat: create first endpoint and database connection via .env

---
+++ src/main/java/br/com/cadastroalunos/controller/AlunoController.java
@@ -0,0 +1,17 @@
+package br.com.cadastroalunos.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/aluno")
+public class AlunoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+}
