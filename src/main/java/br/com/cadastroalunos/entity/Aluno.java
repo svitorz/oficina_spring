3b1299d997cee6b9fe66af70b5820550b85b9ae8
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 09:36:51 -0300

    feat: crate alunos table on postgres database

---
+++ src/main/java/br/com/cadastroalunos/entity/Aluno.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.cadastroalunos.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author vitor
+ */
+@Table
+@Entity
+@Data //gerador de getters e setters 
+@AllArgsConstructor // gerar construtor com todos os argumentos
+@NoArgsConstructor // gerar construtor com nenhum argumento
+ public class Aluno {
+
+    @Id // chave primária
+    @GeneratedValue(strategy=GenerationType.IDENTITY)  // tipo de chave primária
+    Long matricula;
+
+    @Column(
+        unique = true,
+        nullable= false
+    )
+    String cpf_aluno;
+
+    String nome_aluno, telefoneAluno, emailAluno, data_nasc;
+}
