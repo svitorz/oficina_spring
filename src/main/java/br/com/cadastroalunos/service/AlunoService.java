3e386ce6de1a1bc10e84bfcdef4e466d65a83996
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 10:34:42 -0300

    add: create save method in AlunoService

---
+++ src/main/java/br/com/cadastroalunos/service/AlunoService.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package br.com.cadastroalunos.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cadastroalunos.entity.Aluno;
+import br.com.cadastroalunos.repository.AlunoRepository;
+
+/**
+ *
+ * @author vitor
+ */
+@Service
+public class AlunoService {
+
+    @Autowired
+    AlunoRepository alunoRepository;
+
+    public List<Aluno> getAll(){
+        return alunoRepository.findAll();
+    }
+
+    public Aluno createAluno(Aluno aluno){
+        return alunoRepository.save(aluno);
+    } 
+}
