792903964ca45193b4322d37b3617f52a5bf7daa
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 09:08:42 -0300

    change: gitignore

--- src/test/java/br/com/cadastroalunos/CadastroalunosApplicationTests.java
+++ src/test/java/br/com/cadastroalunos/CadastroalunosApplicationTests.java
@@ -1,13 +1,12 @@
 package br.com.cadastroalunos;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CadastroalunosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
