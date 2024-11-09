368db17032b54d64377ec8109019e659119175fe
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 10:35:01 -0300

    add: controller methods

--- src/main/java/br/com/cadastroalunos/controller/AlunoController.java
+++ src/main/java/br/com/cadastroalunos/controller/AlunoController.java
@@ -1,17 +1,35 @@
 package br.com.cadastroalunos.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cadastroalunos.entity.Aluno;
+import br.com.cadastroalunos.service.AlunoService;
+
+
 
 
 @RestController
 @RequestMapping("/aluno")
 public class AlunoController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, World!";
-    }
+    @Autowired
+    AlunoService alunoService;
 
+    @GetMapping("/")
+    public List<Aluno> getAll() {
+        return alunoService.getAll(); 
+    }
+    
+    @PostMapping("/")
+    public Aluno createAluno(@RequestBody Aluno aluno) {
+        return alunoService.createAluno(aluno);
+    }
+    
 }
