09bad1692b4dd4729e20e1beb6de11df73a891a6
Vitor Souza <vitor.fabio.castro@gmail.com>
2024-11-09 09:01:18 -0300

    initial commit

---
+++ src/main/java/br/com/cadastroalunos/CadastroalunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastroalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroalunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cadastroalunos/CadastroalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastroalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
